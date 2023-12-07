4ba949adb006975f5d7c9b013d3007d5c237c64b
Víctor Beis <vbeisg22@fpcoruna.afundacion.org>
2023-12-07 19:25:15 +0100

    Añadido enemeigo 1

---
+++ El_ladron_de_manzanas/scenes/enemy_1.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+var speed = 30.0
+const gravity = 500
+var facing_right = true
+
+func _ready():
+	$AnimatedSprite2D.play("walk")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if (!$RayCast2D.is_colliding() || $RayCast2D2.is_colliding()) && is_on_floor():
+		flip()
+		
+	velocity.x = speed
+	move_and_slide()
+
+func flip():
+	facing_right = !facing_right
+	
+	scale.x = abs(scale.x) * -1
+	if facing_right:
+		speed = abs(speed)
+	else:
+		speed = abs(speed) * -1
